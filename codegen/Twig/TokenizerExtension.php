c6626ef747d5de4c6bd1cab93ec633d94479ba9e
ju1ius <jules.bernable@gmail.com>
2020-03-05 16:59:20 +0100

    adds indent filter to tokenizer generator twig extension

--- codegen/Twig/TokenizerExtension.php
+++ codegen/Twig/TokenizerExtension.php
@@ -3,6 +3,7 @@
 namespace ju1ius\HtmlParser\Codegen\Twig;
 
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class TokenizerExtension extends AbstractExtension
@@ -11,6 +12,13 @@ class TokenizerExtension extends AbstractExtension
     {
         return [
             new TwigFunction('cc_in', [$this, 'currentCharIn']),
+        ];
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('indent', [$this, 'indent']),
         ];
     }
 
@@ -26,5 +34,24 @@ class TokenizerExtension extends AbstractExtension
             $exprs[] = sprintf('$cc === %s', $repr);
         }
         return implode(' || ', $exprs);
+    }
+
+    public function indent(string $input, int $level = 0): string
+    {
+        if (!$level) {
+            return $input;
+        }
+        $indent = str_repeat('    ', $level);
+        $lines = explode("\n", $input);
+        $output = [];
+        foreach ($lines as $line) {
+            if (!ltrim($line)) {
+                $output[] = '';
+            } else {
+                $output[] = $indent . $line;
+            }
+        }
+
+        return implode("\n", $output);
     }
 }
