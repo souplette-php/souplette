4679f4ca13fdd0802fd6e724b45c05492397e610
ju1ius <jules.bernable@gmail.com>
2020-03-07 15:41:38 +0100

    small refact

--- src/Tokenizer/Characters.php
+++ src/Tokenizer/Characters.php
@@ -6,7 +6,7 @@ final class Characters
 {
     const ALPHA = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const NUM = '0123456789';
-    const ALNUM = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const ALNUM = self::ALPHA . self::NUM;
     const HEX = 'abcdefABCDEF0123456789';
     const WHITESPACE = " \n\t\f";
 }
