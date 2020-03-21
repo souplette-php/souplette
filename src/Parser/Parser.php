7f1f65a330a2f1d68c098fa775ef36343d4ffbf3
ju1ius <jules.bernable@gmail.com>
2020-03-21 15:51:53 +0100

    refactors parser encoding handling

--- src/Parser/Parser.php
+++ src/Parser/Parser.php
@@ -27,53 +27,54 @@ final class Parser
 
     public function parse(string $input, ?string $encoding = null): \DOMDocument
     {
-        [$input, $encoding] = $this->preprocessInput($input, $encoding);
-        $tokenizer = new Tokenizer($input);
+        $encoding = $this->detectEncoding($input, $encoding);
+        $converted = $this->preprocessInput($input, $encoding);
+        $tokenizer = new Tokenizer($converted);
         try {
             return $this->treeBuilder->buildDocument($tokenizer, $encoding);
         } catch (EncodingChanged $err) {
             $encoding = $err->getEncoding();
-            if ($encoding->getName() !== EncodingLookup::UTF_8) {
-                $input = InputPreprocessor::convertToUtf8($input, $encoding->getName());
-                $tokenizer = new Tokenizer($input);
-            }
+            $converted = $this->preprocessInput($input, $encoding);
+            $tokenizer = new Tokenizer($converted);
             return $this->treeBuilder->buildDocument($tokenizer, $encoding);
         }
     }
 
     public function parseFragment(\DOMElement $contextElement, string $input, ?string $encoding = null)
     {
-        [$input, $encoding] = $this->preprocessInput($input, $encoding);
-        $tokenizer = new Tokenizer($input);
+        $encoding = $this->detectEncoding($input, $encoding);
+        $converted = $this->preprocessInput($input, $encoding);
+        $tokenizer = new Tokenizer($converted);
         try {
             return $this->treeBuilder->buildFragment($tokenizer, $encoding, $contextElement);
         } catch (EncodingChanged $err) {
             $encoding = $err->getEncoding();
-            if ($encoding->getName() !== EncodingLookup::UTF_8) {
-                $input = InputPreprocessor::convertToUtf8($input, $encoding->getName());
-                $tokenizer = new Tokenizer($input);
-            }
+            $converted = $this->preprocessInput($input, $encoding);
+            $tokenizer = new Tokenizer($converted);
             return $this->treeBuilder->buildFragment($tokenizer, $encoding, $contextElement);
         }
     }
 
-    private function preprocessInput(string $input, ?string $encoding = null)
+    private function detectEncoding(string $input, ?string $override = null): Encoding
     {
-        if ($encoding !== null) {
-            $encoding = Encoding::certain($encoding);
-        } else {
-            if ($sniffed = EncodingSniffer::sniff($input)) {
-                $encoding = Encoding::tentative($encoding);
-            } else {
-                $encoding = Encoding::unknown();
-            }
+        if ($override) {
+            return Encoding::certain($override);
         }
+        if ($sniffed = EncodingSniffer::sniff($input)) {
+            return Encoding::tentative($sniffed);
+        }
+
+        return Encoding::unknown();
+    }
+
+    private function preprocessInput(string $input, Encoding $encoding): string
+    {
         if ($encoding->getName() !== EncodingLookup::UTF_8) {
             $input = InputPreprocessor::convertToUtf8($input, $encoding->getName());
         }
         $input = InputPreprocessor::removeBOM($input);
         $input = InputPreprocessor::normalizeNewlines($input);
 
-        return [$input, $encoding];
+        return $input;
     }
 }
