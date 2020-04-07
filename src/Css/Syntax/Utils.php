f36ad59c6e655e0da2474525509435c232d5c9bb
ju1ius <jules.bernable@gmail.com>
2020-04-07 22:20:20 +0200

    [css-syntax] fixes token representations

---
+++ src/Css/Syntax/Utils.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace JoliPotage\Css\Syntax;
+
+final class Utils
+{
+    private const UNESCAPE_STRING_PATTERN = <<<'REGEXP'
+/
+    \\ (?:
+        (?<unicode> [a-f0-9]{1,6} \s? )
+        | (?<ignored> \n )
+        | (?<any> . )
+    )
+/xi
+REGEXP;
+
+    public static function unescapeString(string $input): string
+    {
+        if (strpos($input, '\\') === false) {
+            return $input;
+        }
+
+        return preg_replace_callback(self::UNESCAPE_STRING_PATTERN, 'self::unescapeCodepointCallback', $input);
+    }
+
+    private static function unescapeCodepointCallback(array $matches): string
+    {
+        if (!empty($matches['ignored'])) {
+            return '';
+        } elseif (!empty($matches['any'])) {
+            return $matches['any'];
+        } elseif (!empty($matches['unicode'])) {
+            $cp = hexdec($matches['unicode']);
+            if ($cp === 0 || $cp > 0x10FFFF || ($cp >= 0xD800 && $cp <= 0xDFFF)) {
+                // null, outside unicode or surrogate
+                return "\u{FFFD}";
+            }
+            return \IntlChar::chr($cp);
+        }
+        return "\u{FFFD}";
+    }
+}
