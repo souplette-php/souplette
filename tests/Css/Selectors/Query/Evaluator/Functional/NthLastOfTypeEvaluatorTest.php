268a32f07e16c94b88c684e0c6fb17ebe398cd3c
ju1ius <jules.bernable@gmail.com>
2021-11-19 00:54:09 +0100

    [css] adds nst(-last)?-of-type selectors tests

---
+++ tests/Css/Selectors/Query/Evaluator/Functional/NthLastOfTypeEvaluatorTest.php
@@ -0,0 +1,94 @@
+<?php declare(strict_types=1);
+
+namespace Souplette\Tests\Css\Selectors\Query\Evaluator\Functional;
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+use Souplette\Css\Selectors\Node\Functional\NthLastOfType;
+use Souplette\Css\Selectors\Node\Functional\NthOfType;
+use Souplette\Css\Selectors\Query\Compiler;
+use Souplette\Css\Selectors\Query\QueryContext;
+use Souplette\Css\Syntax\Node\AnPlusB;
+use Souplette\Tests\Html\DomBuilder;
+
+final class NthLastOfTypeEvaluatorTest extends TestCase
+{
+    private static function assertMatches(\DOMElement $element, NthLastOfType $selector, bool $expected)
+    {
+        $ctx = QueryContext::of($element);
+        $evaluator = (new Compiler)->compile($selector);
+        Assert::assertSame($expected, $evaluator->matches($ctx, $element));
+    }
+
+    /**
+     * @dataProvider simpleAnPlusBProvider
+     */
+    public function testSimpleAnPlusB(\DOMElement $element, NthLastOfType $selector, bool $expected)
+    {
+        self::assertMatches($element, $selector, $expected);
+    }
+
+    public function simpleAnPlusBProvider(): \Generator
+    {
+        $dom = DomBuilder::create()
+            ->tag('a')->close()
+            ->tag('b')->close()
+            ->tag('a')->close()
+            ->tag('b')->close()
+            ->tag('a')->close()
+            ->tag('b')->close()
+            ->getDocument();
+        $indices = [];
+        $nodes = iterator_to_array($dom->childNodes);
+        foreach (array_reverse($nodes) as $node) {
+            $indices[$node->localName] ??= 1;
+            $b = $indices[$node->localName]++;
+            $key = sprintf('matches %s:nth-last-of-type(%d)', $node->localName, $b);
+            yield $key => [
+                $node,
+                new NthLastOfType(new AnPlusB(0, $b)),
+                true,
+            ];
+        }
+    }
+
+    /**
+     * @dataProvider aNPlusBProvider
+     */
+    public function testAnPlusB(\DOMElement $element, NthLastOfType $selector, bool $expected)
+    {
+        self::assertMatches($element, $selector, $expected);
+    }
+
+    public function aNPlusBProvider(): \Generator
+    {
+        $dom = DomBuilder::create()
+            ->tag('a')->close()
+            ->tag('b')->close()
+            ->tag('a')->close()
+            ->tag('b')->close()
+            ->tag('a')->close()
+            ->tag('b')->close()
+            ->getDocument();
+
+        $provider = static function(int $a, int $b, array $indices) use ($dom) {
+            $selector = new NthLastOfType(new AnPlusB($a, $b));
+            foreach ($dom->childNodes as $index => $node) {
+                $mustMatch = in_array($index, $indices, true);
+                $key = sprintf(
+                    'child n°%d %s %s',
+                    $index,
+                    $mustMatch ? 'matches' : 'does not match',
+                    $selector,
+                );
+                yield $key => [$node, $selector, $mustMatch];
+            }
+        };
+
+        yield from $provider(2, 0, [2, 3]);
+        yield from $provider(2, 1, [0, 1, 4, 5]);
+        yield from $provider(3, 0, [0, 1]);
+        yield from $provider(3, 1, [4, 5]);
+        yield from $provider(3, 2, [2, 3]);
+    }
+}
